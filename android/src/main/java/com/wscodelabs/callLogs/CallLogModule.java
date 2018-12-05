a9d0ab47370a20a58ee72e3e3eeb778acd10bfe5
trangdao <trang@trustingsocial.com>
2018-12-05 16:23:45 +0700

    add owner number getPhoneNumbers

--- android/src/main/java/com/wscodelabs/callLogs/CallLogModule.java
+++ android/src/main/java/com/wscodelabs/callLogs/CallLogModule.java
@@ -1,29 +1,47 @@
 package com.wscodelabs.callLogs;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.util.Log;
 import android.provider.CallLog;
 import android.provider.CallLog.Calls;
 import android.database.Cursor;
+
 import java.util.Date;
 import java.lang.*;
+
 import android.content.Context;
+
 import org.json.*;
+import org.w3c.dom.Text;
+
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CallLogModule extends ReactContextBaseJavaModule {
 
+    public static final int LINE1_NUMBER_ID = -1;
     private Context context;
 
     public CallLogModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        this.context= reactContext;
+        this.context = reactContext;
     }
 
     @Override
@@ -34,7 +52,7 @@ public class CallLogModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void fetchFromDate(String from, int size, Promise promise) {
         String selection = CallLog.Calls.DATE + " > ?";
-        String[] selectionArgs = { from };
+        String[] selectionArgs = {from};
         fetch(from, size, selection, selectionArgs, promise);
     }
 
@@ -48,7 +66,7 @@ public class CallLogModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void fetchFrom(String from, int size, Promise promise) {
         String selection = CallLog.Calls.DATE + " < ?";
-        String[] selectionArgs = { from };
+        String[] selectionArgs = {from};
         fetch(from, size, selection, selectionArgs, promise);
     }
 
@@ -57,34 +75,90 @@ public class CallLogModule extends ReactContextBaseJavaModule {
         long currentTime = System.currentTimeMillis();
         String from = String.valueOf(currentTime);
         String selection = CallLog.Calls.NUMBER + " = ?";
-        String[] selectionArgs = { number };
+        String[] selectionArgs = {number};
         fetch(from, size, selection, selectionArgs, promise);
     }
 
     @ReactMethod
     public void fetchForNumberFrom(String number, String from, int size, Promise promise) {
         String selection = CallLog.Calls.NUMBER + " = ? AND " + CallLog.Calls.DATE + " < ?";
-        String[] selectionArgs = { number, from };
+        String[] selectionArgs = {number, from};
         fetch(from, size, selection, selectionArgs, promise);
+    }
+
+    @ReactMethod
+    public void getPhoneNumbers(Promise promise) {
+        try {
+            WritableArray writableArray = Arguments.createArray();
+            Map<Integer, String> simPhones = getSimNumbers();
+            for (Map.Entry<Integer, String> simPhone : simPhones.entrySet()) {
+                WritableMap map = Arguments.createMap();
+                map.putDouble("phone_account_id", simPhone.getKey());
+                map.putString("phoneNumber", simPhone.getValue());
+                writableArray.pushMap(map);
+            }
+
+            promise.resolve(writableArray);
+        } catch (Exception ex) {
+            promise.reject(ex);
+        }
+
+    }
+
+    private Map<Integer, String> getSimNumbers() {
+        Map<Integer, String> simPhones = new HashMap<>();
+        try {
+            if (context.checkCallingOrSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED ||
+                    context.checkCallingOrSelfPermission(Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED ||
+                    context.checkCallingOrSelfPermission("android.permission.READ_PHONE_NUMBERS") == PackageManager.PERMISSION_GRANTED) {
+
+                if (Build.VERSION.SDK_INT >= 22) {
+                    List<SubscriptionInfo> subscriptionList = ((SubscriptionManager) context.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE))
+                            .getActiveSubscriptionInfoList();
+                    if (subscriptionList != null) {
+                        for (SubscriptionInfo subscriptionInfo : subscriptionList) {
+                            int subscriptionId = subscriptionInfo.getSubscriptionId();
+                            simPhones.put(subscriptionId, subscriptionInfo.getNumber());
+                            Log.e("CallLogItem", subscriptionId + "");
+                        }
+                    }
+                }
+
+                TelephonyManager telMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+                String number = telMgr.getLine1Number();
+                simPhones.put(LINE1_NUMBER_ID, number);
+            }
+        } catch (Exception ex) {
+
+        }
+        return simPhones;
     }
 
     private String getValueAtColumn(Cursor cursor, int columnIndex, String columnName) {
         try {
             return cursor.getString(columnIndex);
-        } 
-        catch (Exception e) {
+        } catch (Exception e) {
             Log.d("CallLogModule", "could not fetch value at column" + columnName);
             return "";
         }
     }
 
     private void fetch(String from, int size, String selection, String[] selectionArgs, Promise promise) {
+
+        Map<Integer, String> phoneNumbers = getSimNumbers();
+
+        if (phoneNumbers.size() == 0) {
+            promise.resolve("[]");
+            Log.e("CallLogModule", "Can't get sim numbers");
+            return;
+        }
+
         Cursor cursor = this.context.getContentResolver().query(
-            CallLog.Calls.CONTENT_URI,
-            null,
-            selection,
-            selectionArgs,
-            CallLog.Calls.DATE + " DESC");
+                CallLog.Calls.CONTENT_URI,
+                null,
+                selection,
+                selectionArgs,
+                CallLog.Calls.DATE + " DESC");
 
         if (cursor == null) {
             promise.resolve("[]");
@@ -92,10 +166,10 @@ public class CallLogModule extends ReactContextBaseJavaModule {
         }
 
         int number = cursor.getColumnIndex(CallLog.Calls.NUMBER);
-        int viaNumber = cursor.getColumnIndex(Calls.VIA_NUMBER);
+        int phoneAccountId = cursor.getColumnIndex(Calls.PHONE_ACCOUNT_ID);
         int type = cursor.getColumnIndex(CallLog.Calls.TYPE);
         int date = cursor.getColumnIndex(CallLog.Calls.DATE);
-        int duration = cursor.getColumnIndex(CallLog.Calls.DURATION);  
+        int duration = cursor.getColumnIndex(CallLog.Calls.DURATION);
         int name = cursor.getColumnIndex(CallLog.Calls.CACHED_NAME);
         int photo = cursor.getColumnIndex(CallLog.Calls.CACHED_PHOTO_URI);
 
@@ -106,7 +180,7 @@ public class CallLogModule extends ReactContextBaseJavaModule {
             String callName = getValueAtColumn(cursor, name, "name");
             String callPhotoURI = getValueAtColumn(cursor, photo, "photo");
             String phNumber = getValueAtColumn(cursor, number, "number");
-            String viaPhNumber = getValueAtColumn(cursor, viaNumber, "viaNumber");
+            String phoneAccount = getValueAtColumn(cursor, phoneAccountId, Calls.PHONE_ACCOUNT_ID);
             String callType = getValueAtColumn(cursor, type, "type");
             String callDate = getValueAtColumn(cursor, date, "date");
             Date callDayTime = new Date(Long.valueOf(callDate));
@@ -114,22 +188,24 @@ public class CallLogModule extends ReactContextBaseJavaModule {
             String dir = null;
             int dircode = Integer.parseInt(callType);
             switch (dircode) {
-            case CallLog.Calls.OUTGOING_TYPE:
-                dir = "OUTGOING";
-                break;
-            case CallLog.Calls.INCOMING_TYPE:
-                dir = "INCOMING";
-                break;
+                case CallLog.Calls.OUTGOING_TYPE:
+                    dir = "OUTGOING";
+                    break;
+                case CallLog.Calls.INCOMING_TYPE:
+                    dir = "INCOMING";
+                    break;
 
-            case CallLog.Calls.MISSED_TYPE:
-                dir = "MISSED";
-                break;
+                case CallLog.Calls.MISSED_TYPE:
+                    dir = "MISSED";
+                    break;
             }
 
             JSONObject callObj = new JSONObject();
-            try{
-                callObj.put("phoneNumber",phNumber);
-                callObj.put("viaPhoneNumber",viaPhNumber);
+            try {
+                callObj.put("phoneNumber", phNumber);
+
+                String myPhone = getMyPhone(phoneNumbers, phoneAccount);
+                callObj.put("phoneAccount", myPhone);
                 callObj.put("callType", dir);
                 callObj.put("callDate", callDate);
                 callObj.put("callDuration", callDuration);
@@ -137,8 +213,8 @@ public class CallLogModule extends ReactContextBaseJavaModule {
                 callObj.put("name", callName);
                 callObj.put("photoURI", callPhotoURI);
                 callArray.put(callObj);
-            }
-            catch(JSONException e){
+//                Log.e("CallLogItem", callObj.toString());
+            } catch (JSONException e) {
                 promise.reject(e);
             }
 
@@ -147,5 +223,18 @@ public class CallLogModule extends ReactContextBaseJavaModule {
 
         cursor.close();
         promise.resolve(callArray.toString());
+    }
+
+    private String getMyPhone(Map<Integer, String> phoneNumbers, String phoneAccount) {
+        String myPhone = "";
+        try {
+            myPhone = phoneNumbers.get(Integer.valueOf(phoneAccount));
+        } catch (Exception ex) {
+//            Log.e("CallLogModule", "Parse phoneAccountId error");
+        }
+        if (TextUtils.isEmpty(myPhone)) {
+            myPhone = phoneNumbers.get(LINE1_NUMBER_ID);
+        }
+        return myPhone;
     }
 }
