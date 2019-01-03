39ccc742b6735412549d41c68d7d14e96472135e
trangdao <trang@trustingsocial.com>
2019-01-03 16:45:27 +0700

    add fetch function for native code

--- android/src/main/java/com/wscodelabs/callLogs/CallLogModule.java
+++ android/src/main/java/com/wscodelabs/callLogs/CallLogModule.java
@@ -8,10 +8,10 @@ import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
-import android.provider.CallLog;
 import android.provider.CallLog.Calls;
 import android.database.Cursor;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.lang.*;
 
@@ -36,7 +36,7 @@ import java.util.Map;
 
 public class CallLogModule extends ReactContextBaseJavaModule {
 
-    public static final int LINE1_NUMBER_ID = -1;
+    private static final int LINE1_NUMBER_ID = -1;
     private Context context;
 
     public CallLogModule(ReactApplicationContext reactContext) {
@@ -109,7 +109,12 @@ public class CallLogModule extends ReactContextBaseJavaModule {
         } catch (Exception ex) {
             promise.reject(ex);
         }
+    }
 
+    public List<CallLog> fetchCallLogBatch(String fromDateTimestamp, int size) {
+        String selection = CallLog.Calls.DATE + " > ?";
+        String[] selectionArgs = {fromDateTimestamp};
+        return fetch(size, selection, selectionArgs, true);
     }
 
     private Map<Integer, String> getSimNumbers() {
@@ -153,6 +158,78 @@ public class CallLogModule extends ReactContextBaseJavaModule {
             Log.d("CallLogModule", "could not fetch value at column" + columnName);
             return "";
         }
+    }
+
+    private List<CallLog> fetch(int size, String selection, String[] selectionArgs, Boolean isAscending) {
+        List<CallLog> callLogs = new ArrayList<>();
+        Map<Integer, String> phoneNumbers = getSimNumbers();
+
+        Cursor cursor = this.context.getContentResolver().query(
+                CallLog.Calls.CONTENT_URI,
+                null,
+                selection,
+                selectionArgs,
+                CallLog.Calls.DATE + (isAscending ? " ASC" : " DESC"));
+
+        if (cursor == null) {
+            return callLogs;
+        }
+
+        int number = cursor.getColumnIndex(CallLog.Calls.NUMBER);
+        int phoneAccountId = cursor.getColumnIndex(Calls.PHONE_ACCOUNT_ID);
+        int type = cursor.getColumnIndex(CallLog.Calls.TYPE);
+        int date = cursor.getColumnIndex(CallLog.Calls.DATE);
+        int duration = cursor.getColumnIndex(CallLog.Calls.DURATION);
+        int name = cursor.getColumnIndex(CallLog.Calls.CACHED_NAME);
+        int photo = cursor.getColumnIndex(CallLog.Calls.CACHED_PHOTO_URI);
+
+        int count = 0;
+
+        while (count < size && cursor.moveToNext()) {
+            try {
+                String callName = getValueAtColumn(cursor, name, "name");
+                String callPhotoURI = getValueAtColumn(cursor, photo, "photo");
+                String phNumber = getValueAtColumn(cursor, number, "number");
+                String phoneAccount = getValueAtColumn(cursor, phoneAccountId, Calls.PHONE_ACCOUNT_ID);
+                String callType = getValueAtColumn(cursor, type, "type");
+                String callDate = getValueAtColumn(cursor, date, "date");
+                String callDuration = getValueAtColumn(cursor, duration, "duration");
+                String dir = null;
+
+                int dircode = Integer.parseInt(callType);
+                switch (dircode) {
+                    case CallLog.Calls.OUTGOING_TYPE:
+                        dir = "OUTGOING";
+                        break;
+                    case CallLog.Calls.INCOMING_TYPE:
+                        dir = "INCOMING";
+                        break;
+
+                    case CallLog.Calls.MISSED_TYPE:
+                        dir = "MISSED";
+                        break;
+                }
+
+                CallLog callLog = new CallLog();
+                callLog.setPhoneNumber(phNumber);
+                String myPhone = getMyPhone(phoneNumbers, phoneAccount);
+                callLog.setPhoneAccount(myPhone);
+                callLog.setCallType(dir);
+                callLog.setCallDate(Long.valueOf(callDate));
+                callLog.setCallDuration(callDuration);
+                callLog.setName(callName);
+                callLog.setPhotoURI(callPhotoURI);
+
+                callLogs.add(callLog);
+            } catch (Exception ex) {
+                Log.e("CallLogModule", ex.getMessage(), ex);
+            }
+
+            count++;
+        }
+
+        cursor.close();
+        return callLogs;
     }
 
     private void fetch(String from, int size, String selection, String[] selectionArgs, Boolean isAscending, Promise promise) {
@@ -223,13 +300,14 @@ public class CallLogModule extends ReactContextBaseJavaModule {
             } catch (JSONException e) {
                 promise.reject(e);
             }
-
             count++;
         }
 
         cursor.close();
         promise.resolve(callArray.toString());
     }
+
+
 
     private String getMyPhone(Map<Integer, String> phoneNumbers, String phoneAccount) {
         String myPhone = "";
@@ -246,5 +324,72 @@ public class CallLogModule extends ReactContextBaseJavaModule {
             myPhone = "";
         }
         return myPhone;
+    }
+
+    public static class CallLog {
+
+        private String phoneNumber;
+        private String phoneAccount;
+        private String callType;
+        private long callDate;
+        private String callDuration;
+        private String name;
+        private String photoURI;
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getPhoneAccount() {
+            return phoneAccount;
+        }
+
+        public void setPhoneAccount(String phoneAccount) {
+            this.phoneAccount = phoneAccount;
+        }
+
+        public String getCallType() {
+            return callType;
+        }
+
+        public void setCallType(String callType) {
+            this.callType = callType;
+        }
+
+        public long getCallDate() {
+            return callDate;
+        }
+
+        public void setCallDate(long callDate) {
+            this.callDate = callDate;
+        }
+
+        public String getCallDuration() {
+            return callDuration;
+        }
+
+        public void setCallDuration(String callDuration) {
+            this.callDuration = callDuration;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getPhotoURI() {
+            return photoURI;
+        }
+
+        public void setPhotoURI(String photoURI) {
+            this.photoURI = photoURI;
+        }
     }
 }
